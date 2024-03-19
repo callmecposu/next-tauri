219d27b35152a36929146542c815f6bf8df51000
callmecposu <pc.cposu@gmail.com>
2024-03-18 20:42:34 -0400

    tauri command with arguments

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,10 +6,15 @@ fn say_hello() -> String {
   return "Hello from Rust!".into();
 }
 
+#[tauri::command]
+fn greet(name:String) -> String {
+  return format!("Hello, {}!", name);
+}
+
 
 fn main() {
   tauri::Builder::default()
-  .invoke_handler(tauri::generate_handler![say_hello])
+  .invoke_handler(tauri::generate_handler![say_hello, greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,40 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
-import {invoke} from "@tauri-apps/api/tauri"
+import { invoke } from "@tauri-apps/api/tauri";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+    const [helloMsg, setHelloMsg] = useState("");
+    const [myName, setMyName] = useState('')
+    const [greetingMsg, setGreetingMsg] = useState('')
 
-  const [helloMsg, setHelloMsg] = useState('')
+    useEffect(() => {
+        invoke("say_hello").then((result) => {
+            setHelloMsg(result);
+        });
+    }, []);
 
-  useEffect(() => {
-    invoke('say_hello').then(result => {
-      setHelloMsg(result)
-    })
-  }, [])
-
-  return (
-    <div className="text-xl">
-      {helloMsg}
-    </div>
-  );
+    return (
+        <div className="m-4">
+            <div className="text-xl mb-8">{helloMsg}</div>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Type here"
+                    class="input input-bordered w-full max-w-xs"
+                    onChange={(e) => {setMyName(e.target.value)}}
+                    onKeyDown={(e) => {
+                      if (e.key == 'Enter'){
+                        invoke('greet', {name: myName}).then(result => {
+                          setGreetingMsg(result)
+                        })
+                      }
+                    }}
+                />
+            </div>
+            <div className="mt-8 w-full text-center text-3xl">{greetingMsg}</div>
+        </div>
+    );
 }
